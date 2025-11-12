048bdb2633fbb500cae60fe692d4d154010a6828
janjaw007 <chayanit.kitm@gmail.com>
2025-11-12 20:47:32 +0700

    add new section and finish question1

---
+++ Introduction-Comput-Programming/LiveTest_PracticeQuestion/question1/script.js
@@ -0,0 +1,9 @@
+//1. create function return last char
+function findLastChar(string) {
+  return string.slice(-1);
+}
+
+console.log(findLastChar("WeStride"));
+console.log(findLastChar("Zoo"));
+console.log(findLastChar("Student"));
+console.log(findLastChar("Ox"));
