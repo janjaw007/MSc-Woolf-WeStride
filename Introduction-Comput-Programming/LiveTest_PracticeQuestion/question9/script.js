7c821a004038d721f7fae863ed696ea56fed42ef
janjaw007 <chayanit.kitm@gmail.com>
2025-11-13 22:00:17 +0700

    upgrade question9

--- Introduction-Comput-Programming/LiveTest_PracticeQuestion/question9/script.js
+++ Introduction-Comput-Programming/LiveTest_PracticeQuestion/question9/script.js
@@ -1,15 +1,19 @@
-alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
-
 function decrypt(string) {
+  const alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
   let realString = [];
+
   for (const char of string) {
-    let realCharIndex = (alphabet.indexOf(char.toLowerCase()) + 13) % 26;
-    let realChar = alphabet[realCharIndex];
-    char == char.toLowerCase()
-      ? realString.push(realChar.toLowerCase())
-      : realString.push(realChar.toUpperCase());
+    if (!alphabet.includes(char.toLowerCase())) {
+      realString.push(char);
+    } else {
+      let realCharIndex = (alphabet.indexOf(char.toLowerCase()) + 13) % 26;
+      let realChar = alphabet[realCharIndex];
+      char == char.toLowerCase()
+        ? realString.push(realChar.toLowerCase())
+        : realString.push(realChar.toUpperCase());
+    }
   }
   return realString.join("");
 }
 
-console.log(decrypt("AbC"));
+console.log(decrypt("Hello World!"));
