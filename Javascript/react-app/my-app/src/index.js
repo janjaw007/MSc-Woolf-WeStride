67abb8f29425507c637142f8eb08be69d7c2e1e6
Chayanit Kitmek <chayanit.kitm@gmail.com>
2025-12-15 20:48:40 +0700

    learned {} to use javascript

--- Javascript/react-app/my-app/src/index.js
+++ Javascript/react-app/my-app/src/index.js
@@ -10,7 +10,15 @@ const root = ReactDOM.createRoot(el);
 
 //4) create component
 function App() {
-  return <h1>Hi there!</h1>;
+  const text = "jjao";
+  //const date = new Date();
+  //const time = date.toLocaleTimeString();
+  return (
+    <div>
+      <h1>Hi {text}!</h1>
+      <h2>{new Date().toLocaleTimeString()}</h2>
+    </div>
+  );
 }
 //5) show the component on the screen
 root.render(<App />);
