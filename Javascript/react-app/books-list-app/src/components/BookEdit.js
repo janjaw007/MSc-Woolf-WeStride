5410c58f0295cbe8edc9d8c7275718c380aefa91
Chayanit Kitmek <chayanit.kitm@gmail.com>
2025-12-31 00:29:50 +0700

    added: img and finish react forms

--- Javascript/react-app/books-list-app/src/components/BookEdit.js
+++ Javascript/react-app/books-list-app/src/components/BookEdit.js
@@ -1,5 +1,24 @@
-function BookEdit() {
-  return <div></div>;
+import { useState } from "react";
+
+function BookEdit({ book, onEdit, onSubmit }) {
+  const [title, setTitle] = useState(book.title);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    onSubmit(book.id, title);
+  }
+
+  return (
+    <form className="book-edit" onSubmit={handleSubmit}>
+      <label>Title</label>
+      <input
+        className="input"
+        onChange={(e) => setTitle(e.target.value)}
+        value={title}
+      />
+      <button className="button is-primary">Save</button>
+    </form>
+  );
 }
 
 export default BookEdit;
