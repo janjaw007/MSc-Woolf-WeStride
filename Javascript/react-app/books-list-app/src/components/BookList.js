f60b14cbf19a892df363f5042d5060eac0b32d03
Chayanit Kitmek <chayanit.kitm@gmail.com>
2026-01-03 00:51:18 +0700

    added:context

--- Javascript/react-app/books-list-app/src/components/BookList.js
+++ Javascript/react-app/books-list-app/src/components/BookList.js
@@ -1,12 +1,23 @@
+import { useContext } from "react";
 import BookShow from "./BookShow";
+import BooksContext from "../context/books";
+
 function BookList({ books, onDelete, onEdit }) {
+  const { count, incrementCount } = useContext(BooksContext);
+
   const renderedBooks = books.map((book) => {
     return (
       <BookShow key={book.id} book={book} onDelete={onDelete} onEdit={onEdit} />
     );
   });
 
-  return <div className="book-list">{renderedBooks}</div>;
+  return (
+    <div className="book-list">
+      {count}
+      <button onClick={incrementCount}>Click</button>
+      {renderedBooks}
+    </div>
+  );
 }
 
 export default BookList;
