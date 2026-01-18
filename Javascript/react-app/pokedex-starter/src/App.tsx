e3e60ccba0a47b9f57ea1830610f1493e0b0a778
Chayanit Kitmek <chayanit.kitm@gmail.com>
2026-01-19 00:14:45 +0700

    setup routes

--- Javascript/react-app/pokedex-starter/src/App.tsx
+++ Javascript/react-app/pokedex-starter/src/App.tsx
@@ -1,5 +1,20 @@
+import { createBrowserRouter, RouterProvider } from "react-router";
+import HomePage from "./pages/home/HomePage";
+import DetailPage from "./pages/detail/DetailPage";
+
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      Component: HomePage,
+    },
+    {
+      path: "/detail",
+      Component: DetailPage,
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;
