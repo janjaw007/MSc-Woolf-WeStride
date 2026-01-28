7f767565f03d92e69f9e7117267ce04eefd6e7fb
Chayanit Kitmek <chayanit.kitm@gmail.com>
2026-01-29 00:26:42 +0700

    finish pokemon detail part1

--- Javascript/react-app/pokedex-starter/src/pages/detail/DetailPage.tsx
+++ Javascript/react-app/pokedex-starter/src/pages/detail/DetailPage.tsx
@@ -20,11 +20,19 @@ function DetailPage() {
     const response = await pokemonDetailService.getPokemonDetail(name);
     if (response.status === 200) {
       const responseResults = response.data;
-      setPokemon({
-        data: responseResults,
-        loading: false,
-        error: null,
-      });
+
+      if (responseResults) {
+        setPokemon({
+          data: {
+            ...responseResults,
+            image:
+              responseResults.sprites.other.dream_world.front_default ||
+              responseResults.sprites.other["official-artwork"].front_default,
+          },
+          loading: false,
+          error: null,
+        });
+      }
     } else {
       setPokemon({
         data: undefined,
@@ -47,8 +55,44 @@ function DetailPage() {
           alt=""
         />
       </div>
-      <div className="grid grid-cols-1 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2  gap-[20px] mt-[40px] ">
-        {pokemon.data && <PokemonCard data={pokemon.data} />}
+      <div className="w-[90%] max-w-[600px] m-[auto]">
+        {pokemon.data && (
+          <div className="p-2  text-white flex flex-col h-full  rounded-base shadow-xs dark:border-gray-70 m-[auto]">
+            <div className="bg-center aspect-square w-full bg-cover rounded-[20px] relative h-[400px]">
+              <img
+                className="absolute h-[auto] max-h-[400px] w-full translate-x-[-50%] translate-y-[-50%] top-[50%] left-[50%]" // 1. บังคับความสูงรูป + object-cover
+                src="/images/pokemon_bg.png"
+                alt=""
+              />
+              <img
+                className="absolute  h-[150px] sm:h-[200px] translate-x-[-50%] translate-y-[-50%] top-[50%] left-[50%]" // 1. บังคับความสูงรูป + object-cover
+                src={pokemon.data.image}
+                alt=""
+              />
+            </div>
+
+            <div className=" px-2 mt-1 flex justify-between">
+              <h5 className="capitalize mb-2 text-xl font-semibold tracking-tight text-heading line-clamp-1">
+                {pokemon.data.name}
+              </h5>
+              <h5 className="mb-2 text-xl font-semibold tracking-tight text-heading line-clamp-1">
+                #{pokemon.data.id}
+              </h5>
+            </div>
+
+            <div className="mt-auto px-1 py-2 flex gap-2 justify-end">
+              {pokemon.data.types.map((item) => {
+                return (
+                  <span
+                    className={`badge-type-${item.type.name} rounded-xl text-black px-3 py-2 capitalize`}
+                  >
+                    {item.type.name}
+                  </span>
+                );
+              })}
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
